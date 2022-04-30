23132d78200e511e854ed8cde88c5e5776c2e6e3
SuvrojyotiDey <sammrat.dey.india@gmail.com>
2022-04-30 06:55:25 +0530

    day2Changes

--- reactapp/src/Components/Button/Button.Component.tsx
+++ reactapp/src/Components/Button/Button.Component.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-const Button=(props:any)=>{
-    const {labelData,typeData,buttonCustomStyles}=props;
-    return(
+const Button = (props: any) => {
+    const { labelData, typeData, buttonCustomStyles, buttonValue } = props;
+    return (
         <>
-        <div>
-            <input 
-                type={typeData}
-                value={labelData}
-                className={buttonCustomStyles}
+            <div>
+                <input
+                    type={typeData}
+                    value={labelData}
+                    className={buttonCustomStyles}
+                    onClick={
+                        (e: any) => {
+                            buttonValue(e.target.value)
+                        }
+                    }
             />
-        </div>
+            </div>
         </>
     );
 };
