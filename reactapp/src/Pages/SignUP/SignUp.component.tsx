4bc8c15811370865d14e49962188fa97ce348a53
SuvrojyotiDey <sammrat.dey.india@gmail.com>
2022-05-08 18:59:37 +0530

    day 2 Changes

--- reactapp/src/Pages/SignUP/SignUp.component.tsx
+++ reactapp/src/Pages/SignUP/SignUp.component.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
-import Section from "../../Components/Section/Section.component";
+
 const SignUp = () => {
-    const [data,setData]=useState('');
-    const handleSubmit=()=>{
-    }
     return (<>
         <div>This is signup page</div>
-        <Section></Section>
     </>
     );
 };
