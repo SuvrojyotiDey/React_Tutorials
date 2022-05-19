5db91eff7d47ab4b81c660656d5013c1876e5f5d
SuvrojyotiDey <sammrat.dey.india@gmail.com>
2022-05-19 09:27:33 +0530

    day4Changes

--- reactapp/src/Components/Navbar/Navbar.component.tsx
+++ reactapp/src/Components/Navbar/Navbar.component.tsx
@@ -3,16 +3,27 @@ import { useNavigate } from "react-router-dom";
 import handlingNavigation from "../../Utils/handlingNavigation";
 import styles from "./Navbar.module.css";
 import homeIcon from "../../Assets/homeIcon.svg";
+import menuIcon from "../../Assets/menuIcon.svg";
+import userIcon from "../../Assets/userIcon.png"
 const Navbar = () => {
-    
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     return (
         <>
-            <div className={styles.navContainer}>
-            <div className={styles.leftContainer}>
-            <div><img onClick={(e:any)=>handlingNavigation("/newPage", navigate)} src={homeIcon}/></div>
-            </div>
-            <div className={styles.rightContainer}></div>
+            <div className={styles.navbarContainer}>
+                <div className={styles.leftContainer}>
+                    <div className={styles.homeContainer}>
+                        <img className={styles.home} onClick={(e: any) => handlingNavigation("/newPage", navigate)} src={homeIcon} />
+                    </div>
+                </div>
+                <div className={styles.rightContainer}>
+                    <div className={styles.userContainer}>
+                        <img className={styles.user} src={userIcon} />
+                    </div>
+                    <div className={styles.menuContainer}>
+                        <img className={styles.menu} src={menuIcon} />
+                    </div>
+
+                </div>
             </div>
         </>)
         ;
--- reactapp/src/Pages/NewPage/NewPage.component.tsx
+++ reactapp/src/Pages/NewPage/NewPage.component.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../../Components/Navbar/Navbar.component";
 
 const NewPage=()=>{
     return (
     <>
+        <Navbar/>
         <div>This is new Page</div>
     </>
     );
